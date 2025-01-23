4069e6c9500e2ce904de992e82da3e41c481d6e1
hewansirak <105466147+hewansirak@users.noreply.github.com>
2025-01-23 22:51:45 +0300

    Update file 1/23/2025, 10:51:44 PM

--- 1984-MinimumDifferenceBetweenHighestandLowestofKScores/1984-MinimumDifferenceBetweenHighestandLowestofKScores.py
+++ 1984-MinimumDifferenceBetweenHighestandLowestofKScores/1984-MinimumDifferenceBetweenHighestandLowestofKScores.py
@@ -1,14 +1,14 @@
 class Solution:
     def minimumDifference(self, nums: List[int], k: int) -> int:
         nums.sort()
-        
-        min_variation = float('inf')
-        
+
+        result = float('inf')
+
         for i in range(len(nums) - k + 1):
-            current_variation = nums[i + k - 1] - nums[i]
-            
-            if current_variation < min_variation:
-                min_variation = current_variation
-        
-        return min_variation
+            current = nums[i + k - 1] - nums[i]
+
+            if current < result:
+                result = current
+
+        return result
             
