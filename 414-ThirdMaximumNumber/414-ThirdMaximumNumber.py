36a41bceb64361b6edbcca395d005afa8835da23
hewansirak <105466147+hewansirak@users.noreply.github.com>
2025-03-05 23:05:42 +0300

    File updated at 3/5/2025, 11:05:43 PM

--- 414-ThirdMaximumNumber/414-ThirdMaximumNumber.py
+++ 414-ThirdMaximumNumber/414-ThirdMaximumNumber.py
@@ -1,22 +1,16 @@
 class Solution:
     def thirdMax(self, nums: List[int]) -> int:
-        n = len(nums)
-        
-        for i in range(n - 1):
-            min_index = i  
-            for j in range(i + 1, n):
-                if nums[j] > nums[min_index]:  
-                    min_index = j
-            nums[i], nums[min_index] = nums[min_index], nums[i]  
-        
-        distinct_count = 1
-        third_max = nums[0]  
+        first = second = third = float('-inf')  
 
-        for i in range(1, n):
-            if nums[i] != nums[i - 1]: 
-                distinct_count += 1
-                if distinct_count == 3:
-                    return nums[i]  
-        
-        return nums[0]
+        for num in nums:
+            if num in (first, second, third): 
+                continue
+            if num > first:  
+                first, second, third = num, first, second
+            elif num > second: 
+                second, third = num, second
+            elif num > third: 
+                third = num
+    
+        return third if third != float('-inf') else first
                 
