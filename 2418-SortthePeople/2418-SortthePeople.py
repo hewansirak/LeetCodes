cf303f31d255e9aafc242bd32f2e07b9eea8dd8d
hewansirak <105466147+hewansirak@users.noreply.github.com>
2025-03-06 21:24:08 +0300

    File updated at 3/6/2025, 9:24:09 PM

--- 2418-SortthePeople/2418-SortthePeople.py
+++ 2418-SortthePeople/2418-SortthePeople.py
@@ -3,12 +3,15 @@ class Solution:
         n = len(names)
         people = list(zip(heights, names)) 
         
-        for i in range(n - 1):
-            swapped = False
-            for j in range(n - i - 1):
-                if people[j] < people[j + 1]:
-                    people[j], people[j + 1] = people[j + 1], people[j]  
-                    swapped = True
+        for i in range(1, n):
+            key = people[i]
+            j = i - 1
+
+            while j >= 0 and people[j][0] < key[0]:  
+                people[j + 1] = people[j]
+                j -= 1
+
+            people[j + 1] = key
 
         return [name for _, name in people]
         
