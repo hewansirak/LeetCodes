e903c048a734c1e4b640f0c2aa4a498ab1f8578b
hewansirak <105466147+hewansirak@users.noreply.github.com>
2025-03-15 22:07:28 +0300

    Create file

---
+++ 1887-ReductionOperationstoMaketheArrayElementsEqual/1887-ReductionOperationstoMaketheArrayElementsEqual.py
@@ -0,0 +1,12 @@
+class Solution:
+    def reductionOperations(self, nums: List[int]) -> int:
+        nums.sort()  
+        n = len(nums)
+        sum_val = 0
+        temp = 0
+
+        for i in range(1, n):
+            if nums[i] != nums[i - 1]:
+                temp += 1  
+            sum_val += temp  
+        return sum_val 
