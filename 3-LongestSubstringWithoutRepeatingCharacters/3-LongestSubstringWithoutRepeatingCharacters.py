72517c21b5e04565a00b6c8650319d1d103d7fad
hewansirak <105466147+hewansirak@users.noreply.github.com>
2025-03-18 21:42:50 +0300

    File updated at 3/18/2025, 9:42:50 PM

--- 3-LongestSubstringWithoutRepeatingCharacters/3-LongestSubstringWithoutRepeatingCharacters.py
+++ 3-LongestSubstringWithoutRepeatingCharacters/3-LongestSubstringWithoutRepeatingCharacters.py
@@ -1,17 +1,16 @@
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-        char_index_map = {}
+        charIndex = {}
         max_length = 0
         left = 0
         
         for right in range(len(s)):
             current_char = s[right]
             
-            if current_char in char_index_map and char_index_map[current_char] >= left:
-                left = char_index_map[current_char] + 1
+            if current_char in charIndex and charIndex[current_char] >= left:
+                left = charIndex[current_char] + 1
             
-            char_index_map[current_char] = right
-            
+            charIndex[current_char] = right
             max_length = max(max_length, right - left + 1)
         
         return max_length
