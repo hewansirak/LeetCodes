5f36d0a4be3577b9cc942500d8e4be83722490c7
hewansirak <105466147+hewansirak@users.noreply.github.com>
2025-04-23 23:41:35 +0300

    Create file

---
+++ 20-ValidParentheses/20-ValidParentheses.py
@@ -0,0 +1,15 @@
+# Last updated: 4/23/2025, 11:41:33 PM
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []
+        closing = { ")" : "(", "}" : "{", "]" : "["}
+
+        for c in s:
+            if c in closing:
+                if stack and stack[-1] == closing[c]:
+                    stack.pop()
+                else:
+                    return False
+            else:
+                stack.append(c)
+        return True if not stack else False
