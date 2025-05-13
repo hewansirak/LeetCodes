f71be9674b9b484e529eb2ec8a529af2a3c2cd3d
hewansirak <105466147+hewansirak@users.noreply.github.com>
2025-05-13 16:13:53 +0300

    File updated at 5/13/2025, 4:13:51 PM

--- 875-KokoEatingBananas/875-KokoEatingBananas.py
+++ 875-KokoEatingBananas/875-KokoEatingBananas.py
@@ -1,16 +1,22 @@
-# Last updated: 5/9/2025, 11:26:10 PM
+# Last updated: 5/13/2025, 4:13:51 PM
 class Solution:
     def minEatingSpeed(self, piles: List[int], h: int) -> int:
         low = 1
         high = max(piles)
+        ans = 0
 
         while low <= high:
             mid = low + (high - low) // 2
-            total_hours = sum((pile + mid - 1) // mid for pile in piles)
 
-            if total_hours > h:
-                low = mid + 1
-            else:
+            total_hours = sum(math.ceil(pile / mid) for pile in piles)
+
+            if total_hours <= h:
+                ans = mid
                 high = mid - 1
+            else: 
+                low = mid + 1
+            
+        return ans
 
-        return low
+        
+            
