5db04ae469995a8e5e6afe705e3db6c57195a437
hewansirak <105466147+hewansirak@users.noreply.github.com>
2025-06-05 21:43:35 +0300

    Create 47-PermutationsII

---
+++ 47-PermutationsII/47-PermutationsII.py
@@ -0,0 +1,18 @@
+# Last updated: 6/5/2025, 9:43:34 PM
+class Solution:
+    def permuteUnique(self, nums: List[int]) -> List[List[int]]:
+        nums.sort()
+        res = [[]]
+        
+        for num in nums:
+            n = len(res)
+            new_res = []
+            for i in range(n):
+                prev = res[i]
+                for j in range(len(prev)+1):
+                    if j > 0 and prev[j-1] == num:
+                        break
+                    new1 = prev[:j] + [num] + prev[j:]
+                    new_res.append(new1)
+            res = new_res
+        return res
