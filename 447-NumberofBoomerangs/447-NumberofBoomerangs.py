976d1c132a213d9f36728ba61825f9ba8bf812fb
hewansirak <105466147+hewansirak@users.noreply.github.com>
2025-06-14 05:28:35 +0300

    Create 447-NumberofBoomerangs

---
+++ 447-NumberofBoomerangs/447-NumberofBoomerangs.py
@@ -0,0 +1,14 @@
+# Last updated: 6/14/2025, 5:28:35 AM
+class Solution:
+    def numberOfBoomerangs(self, points: List[List[int]]) -> int:
+        boomerangs = 0
+        for p1 in points:
+            distances = defaultdict(int)
+            for p2 in points:
+                if p1 == p2:
+                    continue
+                dist = (p1[0] - p2[0]) ** 2 + (p1[1] - p2[1]) ** 2
+                distances[dist] += 1
+            for dist in distances.values():
+                boomerangs += dist * (dist - 1)
+        return boomerangs
