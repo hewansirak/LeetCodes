a3edc7e390a580f85102beecf5711989ca0787e5
hewansirak <105466147+hewansirak@users.noreply.github.com>
2025-06-19 20:17:50 +0300

    File updated at 6/19/2025, 8:17:50 PM

--- 1887-ReductionOperationstoMaketheArrayElementsEqual/1887-ReductionOperationstoMaketheArrayElementsEqual.py
+++ 1887-ReductionOperationstoMaketheArrayElementsEqual/1887-ReductionOperationstoMaketheArrayElementsEqual.py
@@ -1,4 +1,4 @@
-# Last updated: 3/24/2025, 9:53:16 PM
+# Last updated: 6/19/2025, 8:17:50 PM
 class Solution:
     def reductionOperations(self, nums: List[int]) -> int:
         nums.sort()  
@@ -10,4 +10,4 @@ class Solution:
             if nums[i] != nums[i - 1]:
                 temp += 1  
             sum_val += temp  
-        return sum_val 
+        return sum_val
