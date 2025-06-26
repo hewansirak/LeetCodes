96858c46ede0f423cb921d08314202d8526b8699
hewansirak <105466147+hewansirak@users.noreply.github.com>
2025-06-26 22:44:38 +0300

    Create 207-CourseSchedule

---
+++ 207-CourseSchedule/207-CourseSchedule.py
@@ -0,0 +1,26 @@
+# Last updated: 6/26/2025, 10:44:38 PM
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        adj = defaultdict(list)
+        for crs, pre in prerequisites:
+            adj[crs].append(pre)
+        visited = set()
+
+        def dfs(crs):
+            if crs in visited:
+                return False
+            if adj[crs] == []:
+                return True
+
+            visited.add(crs)
+            for pre in adj[crs]:
+                if not dfs(pre):
+                    return False
+            visited.remove(crs)
+            adj[crs] = [] 
+            return True
+        
+        for course in range(numCourses):
+            if not dfs(course):
+                return False
+        return True
