22ec315c5cdfbfbde2ad6bdb22d4492c962deda6
hewansirak <105466147+hewansirak@users.noreply.github.com>
2025-06-29 23:59:11 +0300

    File updated at 6/29/2025, 11:59:11 PM

--- 547-NumberofProvinces/547-NumberofProvinces.py
+++ 547-NumberofProvinces/547-NumberofProvinces.py
@@ -1,4 +1,4 @@
-# Last updated: 6/29/2025, 11:58:59 PM
+# Last updated: 6/29/2025, 11:59:11 PM
 class Solution:
     def findCircleNum(self, isConnected: List[List[int]]) -> int:
         def dfs(i):
