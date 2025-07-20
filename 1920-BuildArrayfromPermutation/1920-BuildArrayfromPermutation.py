68dc1141a2039702237067b51317ac4a31d88691
hewansirak <105466147+hewansirak@users.noreply.github.com>
2025-07-20 23:57:52 +0300

    File updated at 7/20/2025, 11:57:52 PM

--- 1920-BuildArrayfromPermutation/1920-BuildArrayfromPermutation.py
+++ 1920-BuildArrayfromPermutation/1920-BuildArrayfromPermutation.py
@@ -1,7 +1,7 @@
+# Last updated: 7/20/2025, 11:57:52 PM
 class Solution:
     def buildArray(self, nums: List[int]) -> List[int]:
         ans = []
         for i in range(len(nums)):
             ans.append(nums[nums[i]])
         return ans
-            
