e0c54aaf359b6c304b01e6774e37a308966c54a0
hewansirak <105466147+hewansirak@users.noreply.github.com>
2025-07-22 21:43:41 +0300

    Create 210-CourseScheduleII

---
+++ 210-CourseScheduleII/210-CourseScheduleII.py
@@ -0,0 +1,29 @@
+# Last updated: 7/22/2025, 9:43:41 PM
+class Solution:
+    def findOrder(self, numCourses, prerequisites):
+        graph = [[] for _ in range(numCourses)]
+        incoming = [0 for _ in range(numCourses)]
+        queue = deque()
+        order = []
+
+        for course, pre in prerequisites:
+            graph[pre].append(course)
+            incoming[course] += 1
+
+        for course in range(numCourses):
+            if incoming[course] == 0:
+                queue.append(course)
+
+        while queue:
+            course = queue.popleft()
+            order.append(course)
+
+            for neighbor in graph[course]:
+                incoming[neighbor] -= 1
+                if incoming[neighbor] == 0:
+                    queue.append(neighbor)
+
+        if len(order) != numCourses:
+            return []
+        else:
+            return order
