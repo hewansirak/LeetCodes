fe5959aaaac4b3aa49377baa9bb737d21a5bdc93
hewansirak <105466147+hewansirak@users.noreply.github.com>
2025-07-28 22:06:35 +0300

    Create 1962-RemoveStonestoMinimizetheTotal

---
+++ 1962-RemoveStonestoMinimizetheTotal/1962-RemoveStonestoMinimizetheTotal.py
@@ -0,0 +1,18 @@
+# Last updated: 7/28/2025, 10:06:34 PM
+class Solution:
+    def minStoneSum(self, piles: List[int], k: int) -> int:
+        total = sum(piles)
+        piles = [-i for i in piles]
+        
+        heapq.heapify(piles)
+
+        while k > 0:
+            temp = heapq.heappop(piles)*-1
+
+            total -= temp//2
+            temp -= temp//2
+            heapq.heappush(piles, temp*-1)
+            k -= 1
+            
+        return total
+            
