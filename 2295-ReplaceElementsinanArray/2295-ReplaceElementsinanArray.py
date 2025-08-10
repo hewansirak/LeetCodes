580161c88eab3e4cf0b99984f9c49f08cd948c94
hewansirak <105466147+hewansirak@users.noreply.github.com>
2025-08-10 23:12:02 +0300

    Create 2295-ReplaceElementsinanArray

---
+++ 2295-ReplaceElementsinanArray/2295-ReplaceElementsinanArray.py
@@ -0,0 +1,10 @@
+# Last updated: 8/10/2025, 11:12:02 PM
+class Solution:
+    def arrayChange(self, nums: List[int], operations: List[List[int]]) -> List[int]:
+        swaps = {}
+        for s, e in reversed(operations):
+            swaps[s] = swaps[e] if e in swaps else e
+        for i, num in enumerate(nums):
+            if num in swaps:
+                nums[i] = swaps[num]
+        return nums
