a32a7b1ab2ac92091190ea42f01ac5b42d67b08d
hewansirak <105466147+hewansirak@users.noreply.github.com>
2025-08-22 22:56:53 +0300

    Create 467-UniqueSubstringsinWraparoundString

---
+++ 467-UniqueSubstringsinWraparoundString/467-UniqueSubstringsinWraparoundString.py
@@ -0,0 +1,20 @@
+# Last updated: 8/22/2025, 10:56:53 PM
+class Solution:
+    def findSubstringInWraproundString(self, s: str) -> int:
+        n = len(s)
+        dp = [0] * 26
+        curr_leng = 1        
+        dp[ord(s[0]) - 97] = 1
+        
+        for i in range(1, n):
+            # zab
+            if (ord(s[i]) - ord(s[i - 1])) % 26 == 1 :
+                curr_leng += 1
+            else:
+                curr_leng = 1
+                
+            idx = ord(s[i]) - 97
+            dp[idx] = max(dp[idx], curr_leng)
+                
+        return sum(dp)
+            
