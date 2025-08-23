7f0a341ef6da6a9dd49424d1154b0f3cad7e092c
hewansirak <105466147+hewansirak@users.noreply.github.com>
2025-08-23 22:59:30 +0300

    Create 518-CoinChangeII

---
+++ 518-CoinChangeII/518-CoinChangeII.py
@@ -0,0 +1,19 @@
+# Last updated: 8/23/2025, 10:59:30 PM
+class Solution:
+    def change(self, amount: int, coins: List[int]) -> int:
+        n = len(coins)
+        dp = [[0 for _ in range(n+1)] for _ in range(amount+1)]
+        
+        for i in range(amount):
+            dp[i][n] = 0
+        for i in range(n+1):
+            dp[0][i] = 1
+        
+        for target in range(1, amount+1):
+            for i in range(n-1,-1,-1):
+                if target - coins[i] < 0:
+                    dp[target][i] = dp[target][i+1]
+                else:
+                    dp[target][i] = dp[target][i+1] + dp[target-coins[i]][i]
+        print(dp)
+        return dp[amount][0]
