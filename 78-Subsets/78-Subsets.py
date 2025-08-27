0240b4a63b9b62057201036e4d995b6aec7c300a
hewansirak <105466147+hewansirak@users.noreply.github.com>
2025-08-27 23:02:45 +0300

    Create 78-Subsets

---
+++ 78-Subsets/78-Subsets.py
@@ -0,0 +1,11 @@
+# Last updated: 8/27/2025, 11:02:45 PM
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        def backtrack(start, path):
+            result.append(path)
+            for i in range(start, len(nums)):
+                backtrack(i + 1, path + [nums[i]])
+
+        result = []
+        backtrack(0, [])
+        return result
