d9a6a7b6ca9a5d5df8301f45df301c23d156b77f
hewansirak <105466147+hewansirak@users.noreply.github.com>
2025-09-04 23:51:05 +0300

    Create 1049-LastStoneWeightII

---
+++ 1049-LastStoneWeightII/1049-LastStoneWeightII.py
@@ -0,0 +1,11 @@
+# Last updated: 9/4/2025, 11:51:04 PM
+class Solution:
+    def lastStoneWeightII(self, stones: List[int]) -> int:
+        prev_state = {0}
+        for elem in stones:
+            state = set()
+            for prev in prev_state:
+                state.add(elem + prev)
+                state.add(abs(prev - elem))
+            prev_state = state
+        return min(prev_state)
