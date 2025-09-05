ce3ab1be8aff435327b224af399eda5aea2f88ca
hewansirak <105466147+hewansirak@users.noreply.github.com>
2025-09-05 22:48:28 +0300

    Create 912-SortanArray

---
+++ 912-SortanArray/912-SortanArray.py
@@ -0,0 +1,26 @@
+# Last updated: 9/5/2025, 10:48:27 PM
+class Solution:
+    def sortArray(self, nums: List[int]) -> List[int]:
+        def divide(nums):
+            if len(nums) <= 1:
+                return nums
+            mid = len(nums) // 2  
+            left = divide(nums[:mid]) 
+            right = divide(nums[mid:])
+            return merge(left, right)
+       
+        def merge(l,r):
+            res = []
+            i = j = 0
+            while i < len(l) and j < len(r):
+                if l[i] < r[j]:
+                    res.append(l[i])
+                    i += 1
+                else:
+                    res.append(r[j])
+                    j += 1
+            res.extend(l[i:])    
+            res.extend(r[j:]) 
+            return res
+
+        return divide(nums)   
