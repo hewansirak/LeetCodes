5f26ca9d9e8a076b256e7966a9d1b3f30d508e7d
hewansirak <105466147+hewansirak@users.noreply.github.com>
2025-09-20 00:02:38 +0300

    Create 2428-MaximumSumofanHourglass

---
+++ 2428-MaximumSumofanHourglass/2428-MaximumSumofanHourglass.py
@@ -0,0 +1,15 @@
+# Last updated: 9/20/2025, 12:02:38 AM
+class Solution:
+    def maxSum(self, grid: List[List[int]]) -> int:
+
+        m, n, ans = len(grid)-1, len(grid[0])-1, 0
+
+        for i, j in product(range(1, m), range(1,n)):
+
+                sm = ( grid[i-1][j-1] + grid[i-1][j] + grid[i-1][j+1]
+                                      + grid[i  ][j] + 
+                       grid[i+1][j-1] + grid[i+1][j] + grid[i+1][j+1] )
+
+                if sm > ans: ans = sm
+
+        return ans
