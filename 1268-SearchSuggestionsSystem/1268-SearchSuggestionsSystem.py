ef2181d658f7d6f5af0e791537f7f50cf1d47b44
hewansirak <105466147+hewansirak@users.noreply.github.com>
2025-09-24 22:13:39 +0300

    Create 1268-SearchSuggestionsSystem

---
+++ 1268-SearchSuggestionsSystem/1268-SearchSuggestionsSystem.py
@@ -0,0 +1,36 @@
+# Last updated: 9/24/2025, 10:13:39 PM
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.end = False
+        self.suggestions = list()
+
+class Solution:
+    def suggestedProducts(self, products: List[str], searchWord: str) -> List[List[str]]:
+        root = TrieNode()
+        products.sort()
+        
+        for product in products:
+            cur = root
+            for letter in product:
+                if letter not in cur.children:
+                    cur.children[letter] = TrieNode()
+                cur = cur.children[letter]
+        
+                if len(cur.suggestions) < 3:
+                    cur.suggestions.append(product)
+            
+            cur.end = True
+            
+        res = [[]] * len(searchWord)
+        
+        cur = root
+        
+        for i in range(len(searchWord)):
+            if searchWord[i] not in cur.children:
+                break
+            
+            cur = cur.children[searchWord[i]]
+            res[i] = cur.suggestions
+        
+        return res
