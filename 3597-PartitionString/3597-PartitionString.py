15766cba5950988a11cdb89ca9f7cbb7e818e497
hewansirak <105466147+hewansirak@users.noreply.github.com>
2025-10-05 23:24:42 +0300

    Create 3597-PartitionString

---
+++ 3597-PartitionString/3597-PartitionString.py
@@ -0,0 +1,38 @@
+# Last updated: 10/5/2025, 11:24:42 PM
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.word_end = 0
+
+class Trie:
+    def __init__(self):
+        self.root = TrieNode()
+
+    def insert(self, word):
+        node = self.root
+        for c in word:
+            if c not in node.children:
+                node.children[c] = TrieNode()
+            node = node.children[c]
+        node.word_end += 1
+
+    def check(self, word):
+        node = self.root
+        for c in word:
+            if c not in node.children:
+                return False
+            node = node.children[c]
+        return node.word_end != 0
+
+class Solution:
+    def partitionString(self, s: str) -> List[str]:
+        trie = Trie()
+        word = ''
+        segments = []
+        for l in s:
+            word += l
+            if not trie.check(word):
+                trie.insert(word)
+                segments.append(word)
+                word = ''
+        return segments
