3eb6b96da30519f0570dd03e478cc87f22481d20
hewansirak <105466147+hewansirak@users.noreply.github.com>
2025-10-12 23:34:00 +0300

    Create 820-ShortEncodingofWords

---
+++ 820-ShortEncodingofWords/820-ShortEncodingofWords.py
@@ -0,0 +1,27 @@
+# Last updated: 10/12/2025, 11:34:01 PM
+class TrieNode:
+    def __init__(self, ch: Optional[str]='#'):
+        self.ch = ch
+        self.cd = {}  # children nodes
+        self.end = False
+        
+class Trie:
+    def __init__(self):
+        self.root = TrieNode()
+    
+    def add(self, word: str) -> None:
+        curr = self.root
+        for i in range(len(word)):
+            if word[~i] not in curr.cd:  # ~i = -i-1
+                curr.cd[word[~i]] = TrieNode(word[~i])
+            curr = curr.cd[word[~i]]
+        curr.end = True  # mark the end of the word
+
+class Solution:
+    def minimumLengthEncoding(self, words: List[str]) -> int:
+        trie = Trie()
+        for word in words:
+            trie.add(word)
+        def dfs(node: TrieNode, curr: int) -> int:
+            return sum(dfs(adj, curr+1) for adj in node.cd.values()) if node.cd else curr
+        return dfs(trie.root, 1)
