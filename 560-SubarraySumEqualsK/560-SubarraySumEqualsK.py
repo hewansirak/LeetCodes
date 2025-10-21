94bebfd9c7d2866c618abd4b4e0efd36ea823a1f
hewansirak <105466147+hewansirak@users.noreply.github.com>
2025-10-21 23:57:37 +0300

    Create 560-SubarraySumEqualsK

---
+++ 560-SubarraySumEqualsK/560-SubarraySumEqualsK.py
@@ -0,0 +1,15 @@
+# Last updated: 10/21/2025, 11:57:35 PM
+class Solution:
+    def subarraySum(self, nums: List[int], k: int) -> int:
+
+        count = 0
+        current = 0
+        prefixSum = {0:1}
+
+        for n in nums:
+            count += n
+            if count - k in prefixSum:
+                current += prefixSum[count - k]
+            prefixSum[count] = prefixSum.get(count, 0) + 1
+
+        return current
