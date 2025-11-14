fefce392ec72479826a39a18591707d9c91edb94
hewansirak <105466147+hewansirak@users.noreply.github.com>
2025-11-14 23:15:47 +0300

    File updated at 11/14/2025, 11:15:47 PM

--- 1823-FindtheWinneroftheCircularGame/1823-FindtheWinneroftheCircularGame.py
+++ 1823-FindtheWinneroftheCircularGame/1823-FindtheWinneroftheCircularGame.py
@@ -1,4 +1,4 @@
-# Last updated: 5/2/2025, 11:07:16 PM
+# Last updated: 11/14/2025, 11:15:47 PM
 class Solution:
     def findTheWinner(self, n: int, k: int) -> int:
         result = 0
@@ -7,7 +7,7 @@ class Solution:
             result = (result + k) % people
         return result + 1
 
-    def helper(n, k):
-        if n == 1:
-            return 0
-        return (helper(n - 1, k) + k) % n
+        def solve(n, k):
+            if n == 1:
+                return 0
+            return (solve(n - 1, k) + k) % n
