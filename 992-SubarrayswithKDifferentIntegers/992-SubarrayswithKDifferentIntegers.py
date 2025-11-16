d1ccbdc3cec97e215247e7bf5fd043a269205a83
hewansirak <105466147+hewansirak@users.noreply.github.com>
2025-11-16 22:33:41 +0300

    Create 992-SubarrayswithKDifferentIntegers

---
+++ 992-SubarrayswithKDifferentIntegers/992-SubarrayswithKDifferentIntegers.py
@@ -0,0 +1,24 @@
+# Last updated: 11/16/2025, 10:33:41 PM
+class Solution:
+    def subarraysWithKDistinct(self, nums: List[int], k: int) -> int:
+        return self.solve(nums, k) - self.solve(nums, k - 1)
+
+    def solve(self, nums, k):
+        count = {}
+        l = 0
+        ans = 0
+
+        for r in range(len(nums)):
+            count[nums[r]] = count.get(nums[r], 0) + 1
+
+            # shrink window 
+            while len(count) > k:
+                count[nums[l]] -= 1
+                if count[nums[l]] == 0:
+                    del count[nums[l]]
+                l += 1
+
+            # no. ending at r
+            ans += (r - l + 1)
+
+        return ans
