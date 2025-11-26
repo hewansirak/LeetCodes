bd9c38d6b9a5a672a83bd5174b504d62e2357048
hewansirak <105466147+hewansirak@users.noreply.github.com>
2025-11-26 22:53:44 +0300

    File updated at 11/26/2025, 10:53:45 PM

--- 779-K-thSymbolinGrammar/779-K-thSymbolinGrammar.py
+++ 779-K-thSymbolinGrammar/779-K-thSymbolinGrammar.py
@@ -1,14 +1,14 @@
-# Last updated: 5/31/2025, 11:29:03 PM
-class Solution:
-    def kthGrammar(self, n: int, k: int) -> int:
-        same = True
-        n = 2**(n - 1)
-
-        while n != 1:
-            n //= 2
-
-            if k > n:
-                k -= n
-                same = not same
-
-        return 0 if same else 1
+# Last updated: 11/26/2025, 10:53:45 PM
+1class Solution:
+2    def kthGrammar(self, n: int, k: int) -> int:
+3        same = True
+4        n = 2**(n - 1)
+5
+6        while n != 1:
+7            n //= 2
+8
+9            if k > n:
+10                k -= n
+11                same = not same
+12
+13        return 0 if same else 1
