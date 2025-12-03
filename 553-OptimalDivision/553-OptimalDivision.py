83fe29017f5c0bda938a8cfa8958aab29389e70f
hewansirak <105466147+hewansirak@users.noreply.github.com>
2025-12-03 22:33:56 +0300

    Create 553-OptimalDivision

---
+++ 553-OptimalDivision/553-OptimalDivision.py
@@ -0,0 +1,10 @@
+# Last updated: 12/3/2025, 10:33:56 PM
+1class Solution:
+2    def optimalDivision(self, nums: List[int]) -> str:
+3        if len(nums) == 1:
+4            return str(nums[0])
+5        elif len(nums) == 2:
+6            return str(nums[0]) + '/' + str(nums[1])
+7        else:
+8            nums_str = list(map(str, nums))
+9            return str(nums[0]) + '/(' + '/'.join(nums_str[1:]) + ')'
