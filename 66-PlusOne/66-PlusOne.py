db841f7feaec063c31008056668891bc17ee4e75
hewansirak <105466147+hewansirak@users.noreply.github.com>
2025-12-06 23:48:04 +0300

    Create 66-PlusOne

---
+++ 66-PlusOne/66-PlusOne.py
@@ -0,0 +1,13 @@
+# Last updated: 12/6/2025, 11:48:03 PM
+1class Solution:
+2    def plusOne(self, digits: List[int]) -> List[int]:
+3        
+4        for i in reversed(range(len(digits))):
+5            
+6            if digits[i] == 9:
+7                digits[i] = 0
+8            else:
+9                digits[i] += 1
+10                return digits
+11        
+12        return [1] + digits
