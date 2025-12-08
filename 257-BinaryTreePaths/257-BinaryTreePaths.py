d5c94d60a8a36b072031aac4ef985a02b516a93a
hewansirak <105466147+hewansirak@users.noreply.github.com>
2025-12-08 21:41:15 +0300

    Create 257-BinaryTreePaths

---
+++ 257-BinaryTreePaths/257-BinaryTreePaths.py
@@ -0,0 +1,25 @@
+# Last updated: 12/8/2025, 9:41:15 PM
+1# Definition for a binary tree node.
+2# class TreeNode:
+3#     def __init__(self, val=0, left=None, right=None):
+4#         self.val = val
+5#         self.left = left
+6#         self.right = right
+7class Solution:
+8    def binaryTreePaths(self, root: Optional[TreeNode]) -> List[str]:
+9        path = []
+10        ans = []
+11
+12        def dfs(root):
+13            if not root:
+14                return
+15            path.append(str(root.val))
+16            if not root.left and not root.right:
+17                ans.append("->".join(path))
+18            else:
+19                dfs(root.left)
+20                dfs(root.right)
+21            path.pop()
+22
+23        dfs(root)
+24        return ans
