d5b64d250fd841f02da6b86dbf8a611564a5eff3
hewansirak <105466147+hewansirak@users.noreply.github.com>
2025-12-11 22:41:18 +0300

    File updated at 12/11/2025, 10:41:17 PM

--- 45-JumpGameII/45-JumpGameII.py
+++ 45-JumpGameII/45-JumpGameII.py
@@ -1,4 +1,4 @@
-# Last updated: 12/11/2025, 10:41:06 PM
+# Last updated: 12/11/2025, 10:41:17 PM
 1class Solution:
 2    def jump(self, nums: List[int]) -> int:
 3        jumps, max_reach, steps = 0, 0, 0
