9de1376de3fbc29bfb3b3b53ab87b14efd933cf8
hewansirak <105466147+hewansirak@users.noreply.github.com>
2025-12-19 22:49:59 +0300

    File updated at 12/19/2025, 10:50:00 PM

--- 2167-MinimumTimetoRemoveAllCarsContainingIllegalGoods/2167-MinimumTimetoRemoveAllCarsContainingIllegalGoods.py
+++ 2167-MinimumTimetoRemoveAllCarsContainingIllegalGoods/2167-MinimumTimetoRemoveAllCarsContainingIllegalGoods.py
@@ -1,4 +1,4 @@
-# Last updated: 12/19/2025, 10:49:50 PM
+# Last updated: 12/19/2025, 10:50:00 PM
 1class Solution:
 2    def minimumTime(self, s: str) -> int:
 3        length, start, res = len(s), 0, len(s)
