2cb5dd50b23260f147ab872b105efa0120b1345e
hewansirak <105466147+hewansirak@users.noreply.github.com>
2025-12-19 22:44:39 +0300

    Create 2167-MinimumTimetoRemoveAllCarsContainingIllegalGoods

---
+++ 2167-MinimumTimetoRemoveAllCarsContainingIllegalGoods/2167-MinimumTimetoRemoveAllCarsContainingIllegalGoods.py
@@ -0,0 +1,10 @@
+# Last updated: 12/19/2025, 10:44:40 PM
+1class Solution:
+2    def minimumTime(self, s: str) -> int:
+3        length, start, res = len(s), 0, len(s)
+4        
+5        for i, c in enumerate(s):
+6            start = min(start + (c == "1") * 2, i + 1)
+7            res = min(res, start + length - 1 - i)
+8        
+9        return res
